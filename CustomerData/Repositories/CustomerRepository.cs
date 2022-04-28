cd9e1b1d304b46410afc117174ffa06181cdb5d5
jaddielorzano-pointwest <jaddie.lorzano@pointwest.com.ph>
2022-04-29 07:44:39 +0800

    Create Repositories

---
+++ CustomerData/Repositories/CustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using CustomerData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerData.Repositories
+{   
+    public interface ICustomerRepository : IBaseRepository<Customer>
+    {
+
+    }
+    public class CustomerRepository
+    {
+
+    }
+}
